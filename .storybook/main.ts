9a098f4433a8a9c654c644829ebb326cbf68b085
Fabrizio Calderan <fabrizio.calderan@proced.it>
2025-10-08 17:52:10 +0200

    feat(storybook): trying to add a view HTML source, but no luck so far

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,11 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-docs"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-docs",
+    "@whitespace/storybook-addon-html",
+  ],
   framework: {
     name: "@storybook/react-vite",
     options: {},
