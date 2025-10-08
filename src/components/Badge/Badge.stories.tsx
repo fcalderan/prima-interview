2e6a33a7cd2f17159bf99e0fcff6748de87b87b8
Fabrizio Calderan <fabrizio.calderan@proced.it>
2025-10-08 14:16:35 +0200

    feat! Badge component

---
+++ src/components/Badge/Badge.stories.tsx
@@ -0,0 +1,28 @@
+import { Meta, StoryFn } from "@storybook/react";
+import { Badge, BadgeProps } from "./Badge";
+
+const meta: Meta<typeof Badge> = {
+  title: "Components/Badge",
+  component: Badge,
+  tags: ["autodocs"],
+  argTypes: {
+    label: {
+      control: "text",
+      description: "Tab label",
+    },
+    variant: {
+      control: { type: "select" },
+      options: ["neutral", "positive", "negative"],
+      description: "Tab variant",
+    },
+  },
+};
+export default meta;
+
+const Template: StoryFn<BadgeProps> = (args: BadgeProps) => <Badge {...args} />;
+
+export const playground: StoryFn<BadgeProps> = Template.bind({});
+playground.args = {
+  label: "Home",
+  variant: "neutral",
+};
---
+++ src/components/Badge/Badge.tsx
@@ -0,0 +1,23 @@
+import styles from "./Badge.module.css";
+
+export interface BadgeProps {
+  /** Badge content, visible to the user */
+  label: string;
+  /** Badge variant: 'neutral', 'positive', o 'negative' */
+  variant?: "neutral" | "positive" | "negative";
+}
+
+export const Badge: React.FC<BadgeProps> = ({ label, variant = "neutral" }) => {
+  const BadgeElement = variant === "neutral" ? "span" : "em";
+  /* I choose to use a <span> element in case of a neutral variant 
+   + and an <em> element for positive and negative variants to
+   + convey a semantic meaning of emphasis.
+   */
+  return (
+    <BadgeElement className={`${styles.badge} ${styles[variant]}`}>
+      {label}
+    </BadgeElement>
+  );
+};
+
+export default Badge;
