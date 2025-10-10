42edb6820c6c25b9f841690cd7168819ec134ad4
Fabrizio Calderan <fabrizio.calderan@proced.it>
2025-10-10 11:37:33 +0200

    Refactoring: applied new Tabpanels component

--- src/components/Tabcontainer/Tabcontainer.stories.tsx
+++ src/components/Tabcontainer/Tabcontainer.stories.tsx
@@ -2,6 +2,7 @@ import { Meta, StoryObj } from "@storybook/react";
 import { Tab } from "../Tab";
 import { Tabs } from "../Tabs";
 import { Tabpanel } from "../Tabpanel";
+import { Tabpanels } from "../Tabpanels";
 import { Tabcontainer } from "./";
 
 const meta: Meta<typeof Tabcontainer> = {
@@ -31,67 +32,70 @@ export const PillTabcontainer: Story = {
         />
       </Tabs>
 
-      <Tabpanel id="tab1">
-        <>
-          <h2>Want to be more</h2>
+      <Tabpanels>
+        <Tabpanel id="tab1">
+          <>
+            <h2>Want to be more</h2>
 
-          <blockquote>
-            <p>
-              Nobody ever got fired for buying an IBM, right? What a fearful way
-              to do business. You've made just enough safe choices to stay
-              alive, but not enough to matter. Is that what you want? You can be
-              more. You want to be more, don't you? The window of opportunity is
-              closing. This is your chance. This is not about not losing. This
-              is about you finally having the confidence to walk out on the
-              ledge and know that you're not going to fall.
-            </p>
-            <cite>Joe McMillan - H&CF S01E01</cite>
-          </blockquote>
-        </>
-      </Tabpanel>
-      <Tabpanel id="tab2">
-        <>
-          <h2>Not the other way around</h2>
-          <blockquote>
-            <p>
-              After IBM, I went across the country just looking for answers. I'd
-              read your article, and I couldn't stop thinking about it. Open
-              architecture, the idea of it. As a way of life. It kept me up at
-              night. It made me that kid again, and I tracked you down to COMDEX
-              and watched as they chased you, too, and your beautiful machine
-              right off the ledge. And you, when I saw you, everything about you
-              threatens people. I thought that maybe we could do this precisely
-              because we're all unreasonable people and progress depends on our
-              changing the world to fit us. Not the other way around. I want to
-              believe that. I must believe that. Tomorrow, I will show up at
-              7:00 A.M. and I will keep going. You don't have to join me, but
-              something tells me you both need this just as much as I do.
-              <cite>Joe McMillan - H&CF S01E02</cite>
-            </p>
-          </blockquote>
-        </>
-      </Tabpanel>
-      <Tabpanel id="tab3">
-        <>
-          <h2>Are we safe?</h2>
-          <blockquote>
-            <p>
-              We live in a world that is constantly shifting, sometimes bringing
-              us together, and sometimes pulling us apart. Who are our friends?
-              Who are our enemies? What's gonna happen to us? Are we safe? What
-              I created has grown. Now, some of you even call MacMillan Utility
-              the gold standard in corporate antivirus protection. But in the
-              meantime, I've been asking myself, "What should the price of
-              security really be? How much should we pay to feel safe?" Wait,
-              here's another one from that same ancient tradition. The whole
-              secret of existence is to have no fear at all. Never fear what
-              will become of you. Depend on no one. Only in the moment that you
-              reject all help are you really free.
-            </p>
-            <cite>Joe McMillan - H&CF S03E01</cite>
-          </blockquote>
-        </>
-      </Tabpanel>
+            <blockquote>
+              <p>
+                Nobody ever got fired for buying an IBM, right? What a fearful
+                way to do business. You've made just enough safe choices to stay
+                alive, but not enough to matter. Is that what you want? You can
+                be more. You want to be more, don't you? The window of
+                opportunity is closing. This is your chance. This is not about
+                not losing. This is about you finally having the confidence to
+                walk out on the ledge and know that you're not going to fall.
+              </p>
+              <cite>Joe McMillan - H&CF S01E01</cite>
+            </blockquote>
+          </>
+        </Tabpanel>
+        <Tabpanel id="tab2">
+          <>
+            <h2>Not the other way around</h2>
+            <blockquote>
+              <p>
+                After IBM, I went across the country just looking for answers.
+                I'd read your article, and I couldn't stop thinking about it.
+                Open architecture, the idea of it. As a way of life. It kept me
+                up at night. It made me that kid again, and I tracked you down
+                to COMDEX and watched as they chased you, too, and your
+                beautiful machine right off the ledge. And you, when I saw you,
+                everything about you threatens people. I thought that maybe we
+                could do this precisely because we're all unreasonable people
+                and progress depends on our changing the world to fit us. Not
+                the other way around. I want to believe that. I must believe
+                that. Tomorrow, I will show up at 7:00 A.M. and I will keep
+                going. You don't have to join me, but something tells me you
+                both need this just as much as I do.
+                <cite>Joe McMillan - H&CF S01E02</cite>
+              </p>
+            </blockquote>
+          </>
+        </Tabpanel>
+        <Tabpanel id="tab3">
+          <>
+            <h2>Are we safe?</h2>
+            <blockquote>
+              <p>
+                We live in a world that is constantly shifting, sometimes
+                bringing us together, and sometimes pulling us apart. Who are
+                our friends? Who are our enemies? What's gonna happen to us? Are
+                we safe? What I created has grown. Now, some of you even call
+                MacMillan Utility the gold standard in corporate antivirus
+                protection. But in the meantime, I've been asking myself, "What
+                should the price of security really be? How much should we pay
+                to feel safe?" Wait, here's another one from that same ancient
+                tradition. The whole secret of existence is to have no fear at
+                all. Never fear what will become of you. Depend on no one. Only
+                in the moment that you reject all help are you really free.
+              </p>
+              <cite>Joe McMillan - H&CF S03E01</cite>
+            </blockquote>
+          </>
+        </Tabpanel>
+      </Tabpanels>
     </Tabcontainer>
   ),
 };
@@ -105,67 +109,70 @@ export const UnderlineTabcontainer: Story = {
         <Tab id="tab3" label="Are we safe?" />
       </Tabs>
 
-      <Tabpanel id="tab1">
-        <>
-          <h2>Want to be more</h2>
+      <Tabpanels>
+        <Tabpanel id="tab1">
+          <>
+            <h2>Want to be more</h2>
 
-          <blockquote>
-            <p>
-              Nobody ever got fired for buying an IBM, right? What a fearful way
-              to do business. You've made just enough safe choices to stay
-              alive, but not enough to matter. Is that what you want? You can be
-              more. You want to be more, don't you? The window of opportunity is
-              closing. This is your chance. This is not about not losing. This
-              is about you finally having the confidence to walk out on the
-              ledge and know that you're not going to fall.
-            </p>
-            <cite>Joe McMillan - H&CF S01E01</cite>
-          </blockquote>
-        </>
-      </Tabpanel>
-      <Tabpanel id="tab2">
-        <>
-          <h2>Not the other way around</h2>
-          <blockquote>
-            <p>
-              After IBM, I went across the country just looking for answers. I'd
-              read your article, and I couldn't stop thinking about it. Open
-              architecture, the idea of it. As a way of life. It kept me up at
-              night. It made me that kid again, and I tracked you down to COMDEX
-              and watched as they chased you, too, and your beautiful machine
-              right off the ledge. And you, when I saw you, everything about you
-              threatens people. I thought that maybe we could do this precisely
-              because we're all unreasonable people and progress depends on our
-              changing the world to fit us. Not the other way around. I want to
-              believe that. I must believe that. Tomorrow, I will show up at
-              7:00 A.M. and I will keep going. You don't have to join me, but
-              something tells me you both need this just as much as I do.
-              <cite>Joe McMillan - H&CF S01E02</cite>
-            </p>
-          </blockquote>
-        </>
-      </Tabpanel>
-      <Tabpanel id="tab3">
-        <>
-          <h2>Are we safe?</h2>
-          <blockquote>
-            <p>
-              We live in a world that is constantly shifting, sometimes bringing
-              us together, and sometimes pulling us apart. Who are our friends?
-              Who are our enemies? What's gonna happen to us? Are we safe? What
-              I created has grown. Now, some of you even call MacMillan Utility
-              the gold standard in corporate antivirus protection. But in the
-              meantime, I've been asking myself, "What should the price of
-              security really be? How much should we pay to feel safe?" Wait,
-              here's another one from that same ancient tradition. The whole
-              secret of existence is to have no fear at all. Never fear what
-              will become of you. Depend on no one. Only in the moment that you
-              reject all help are you really free.
-            </p>
-            <cite>Joe McMillan - H&CF S03E01</cite>
-          </blockquote>
-        </>
-      </Tabpanel>
+            <blockquote>
+              <p>
+                Nobody ever got fired for buying an IBM, right? What a fearful
+                way to do business. You've made just enough safe choices to stay
+                alive, but not enough to matter. Is that what you want? You can
+                be more. You want to be more, don't you? The window of
+                opportunity is closing. This is your chance. This is not about
+                not losing. This is about you finally having the confidence to
+                walk out on the ledge and know that you're not going to fall.
+              </p>
+              <cite>Joe McMillan - H&CF S01E01</cite>
+            </blockquote>
+          </>
+        </Tabpanel>
+        <Tabpanel id="tab2">
+          <>
+            <h2>Not the other way around</h2>
+            <blockquote>
+              <p>
+                After IBM, I went across the country just looking for answers.
+                I'd read your article, and I couldn't stop thinking about it.
+                Open architecture, the idea of it. As a way of life. It kept me
+                up at night. It made me that kid again, and I tracked you down
+                to COMDEX and watched as they chased you, too, and your
+                beautiful machine right off the ledge. And you, when I saw you,
+                everything about you threatens people. I thought that maybe we
+                could do this precisely because we're all unreasonable people
+                and progress depends on our changing the world to fit us. Not
+                the other way around. I want to believe that. I must believe
+                that. Tomorrow, I will show up at 7:00 A.M. and I will keep
+                going. You don't have to join me, but something tells me you
+                both need this just as much as I do.
+                <cite>Joe McMillan - H&CF S01E02</cite>
+              </p>
+            </blockquote>
+          </>
+        </Tabpanel>
+        <Tabpanel id="tab3">
+          <>
+            <h2>Are we safe?</h2>
+            <blockquote>
+              <p>
+                We live in a world that is constantly shifting, sometimes
+                bringing us together, and sometimes pulling us apart. Who are
+                our friends? Who are our enemies? What's gonna happen to us? Are
+                we safe? What I created has grown. Now, some of you even call
+                MacMillan Utility the gold standard in corporate antivirus
+                protection. But in the meantime, I've been asking myself, "What
+                should the price of security really be? How much should we pay
+                to feel safe?" Wait, here's another one from that same ancient
+                tradition. The whole secret of existence is to have no fear at
+                all. Never fear what will become of you. Depend on no one. Only
+                in the moment that you reject all help are you really free.
+              </p>
+              <cite>Joe McMillan - H&CF S03E01</cite>
+            </blockquote>
+          </>
+        </Tabpanel>
+      </Tabpanels>
     </Tabcontainer>
   ),
 };
